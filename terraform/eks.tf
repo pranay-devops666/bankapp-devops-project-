893dd998f748a671718d2cce728a063133ae5bf7
PranayBabuDevOps <118045707+pranay-devops666@users.noreply.github.com>
2026-03-24 09:07:16 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,1 +1,12 @@
-eks.tf
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_eks_cluster" "bankapp" {
+  name     = "bankapp-cluster"
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+    subnet_ids = ["subnet-xxxx", "subnet-yyyy"]
+  }
+}
